2b74227dce25b758e594a346352ef206f3e9d66c
Fengyao Yan <yanfy135@gmail.com>
2024-01-09 13:22:48 -0500

    update run.py with configure output

--- run.py
+++ run.py
@@ -1,6 +1,14 @@
-from configure import model_type, edges
+from configure import model_type, edges, fd
 import subprocess
 import os
+
+print("***************************")
+print(f"model: {model_type}")
+print(f"train_test_dataset: {fd}")
+if model_type in ('gcn', 'pna'):
+    print(f"edges selected: {str(edges)}")
+print("***************************")
+
 
 run_file = list(filter(lambda x:x.split('.')[0]==model_type and x.split('.')[1]=='py', os.listdir('.')))[0]
 if model_type in ('gcn', 'pna'):
